a873a1176b7791222a3ef24931dbd5290e2aed89
nmpereira <nikhilpereira25@gmail.com>
2022-02-22 18:37:53 -0500

    class13

--- homework/class12/class12-materials/class-weekend-boring/js/main.js
+++ homework/class12/class12-materials/class-weekend-boring/js/main.js
@@ -1,10 +1,19 @@
-document.querySelector('#check').addEventListener('click', check)
-
+document.querySelector("#check").addEventListener("click", check);
+let h2 = document.getElementById("placeToSee");
 function check() {
-
-  const day = document.querySelector('#day').value
-
+  let result;
+  const day = document.querySelector("#day").value.toLowerCase();
   //Conditionals go here
-
-
+  if (day === "sunday" || day === "saturday") {
+    result = "Weekend";
+  } else if (day == "monday" || day == "wednesday" || day == "friday") {
+    result = "Boring day";
+  } else if (day == "tuesday" || day == "thursday") {
+    result = "Class day";
+  } else if (day == "") {
+    result = "Add a value";
+  } else {
+    result = "Not a day";
+  }
+  h2.innerHTML = result;
 }
